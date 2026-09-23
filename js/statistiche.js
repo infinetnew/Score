803e3ff79a57963a9c52faa6cef02e98a98368b7
Daniel Caruso <infinetnew@gmail.com>
2026-09-23 22:53:40 +0200

    Update statistiche.js

--- js/statistiche.js
+++ js/statistiche.js
@@ -1,1 +1,4 @@
-
+document.getElementById('open_stats').addEventListener('click', () => {
+    document.getElementById('home_screen').style.display = 'none';
+    document.getElementById('stats_screen').style.display = 'flex';
+});
