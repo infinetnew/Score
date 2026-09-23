d5aebe9ccce632d3d03bd247f2528d9969fe25e4
Daniel Caruso <infinetnew@gmail.com>
2026-09-23 23:19:54 +0200

    Update statistiche.js

--- js/statistiche.js
+++ js/statistiche.js
@@ -43,6 +43,32 @@ async function loadStats() {
     }
 
     console.log('Utente statistiche:', user.id);
+
+    const { data: matches, error: matchesError } = await supabaseClient
+        .from('score_h2h_matches')
+        .select(`
+            id,
+            matchday,
+            player1_id,
+            player2_id,
+            player1_score,
+            player2_score,
+            player1_goals,
+            player2_goals,
+            result,
+            player1_points,
+            player2_points,
+            created_at
+        `)
+        .or(`player1_id.eq.${user.id},player2_id.eq.${user.id}`)
+        .order('created_at', { ascending: false });
+
+    if (matchesError) {
+        console.error('Errore recupero partite statistiche:', matchesError);
+        return;
+    }
+
+    console.log('Partite H2H statistiche:', matches);
 }
 document.getElementById('open_stats').addEventListener('click', async () => {
     await loadStats();
